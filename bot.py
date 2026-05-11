eaca2d69ed08ad1f4f837313edac431b322a28ea
alfredocaso999-tech <alfredocaso999@gmail.com>
2026-05-11 17:04:20 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,12 +2,26 @@ import os
 import telebot
 import random
 from datetime import datetime
+from threading import Thread
+from flask import Flask
 
 TOKEN = os.environ.get("BOT_TOKEN")
 if not TOKEN:
     raise RuntimeError("BOT_TOKEN non impostato")
 
 bot = telebot.TeleBot(TOKEN)
+
+# Questa parte mantiene il bot "vivo" per Render
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot attivo"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+Thread(target=run).start()
 
 @bot.message_handler(commands=['start'])
 def start(m):
